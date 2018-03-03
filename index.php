5d90eb4ab0970b89def850194c3453a1e56e6a1a
weremax <weremax@yandex.ru>
2018-03-03 21:53:06 +0300

    12

--- index.php
+++ index.php
@@ -12,61 +12,47 @@
 
 <!--HEADER-->
 
-
 <section id="header">
 
-    <div class="container col-6">
-
-        <div class="row justify-content-between nav-heigh">
-
-            <div class="navbar-brand">
-                <a href="#">
-                    <img src="image/logo.png" alt="logo">
-                </a>
-            </div>
-
-            <div>
-                <a data-toggle="collapse"
-                   data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent"
-                   aria-expanded="false" aria-label="Toggle navigation">
-                    <img class="nav-button-gam rounded-circle" src="image/menu.svg" alt="menu">
-                </a>
-            </div>
+    <div class="row col-12">
+        <div class="container col-6 col-md-4 text-center">
+            <img id="logo" src="image/logo.png" alt="logo">
+        </div>
+        <div class="container col-2 text-md-right text-right">
+            <a id="menu" href="#" onclick=""><img src="image/menu.svg" alt="menu"></a>
         </div>
 
-        <!--скрытый контент-->
-        <div class="collapse " id="navbarToggleExternalContent">
-            <div class="row justify-content-end">
-                <a href="#">Hello</a>
-                <a href="#about">About</a>
-                <a href="#">Slider</a>
-                <a href="#">Personal</a>
-                <a href="#">Services</a>
-                <a href="#">Contacts</a>
-            </div>
-        </div>
-        <!--скрытый контент-->
     </div>
-    <div class="container text-center hello-text">
-        <h1>HELLO</h1>
-        <h6>WE ARE ONLINE CLINIC</h6>
+    <!--скрытый контент-->
+    <div class="row col-12 text-md-right text-center" id="menu-open">
+        <a href="#">Hello</a>
+        <a href="#about">About</a>
+        <a href="#">Slider</a>
+        <a href="#">Personal</a>
+        <a href="#">Services</a>
+        <a href="#">Contacts</a>
     </div>
+    <!--скрытый контент-->
+    <div class="col-md-6 col-12 container text-center hello-text">
+        <h1><b>HELLO</b></h1>
+        <h6><b>WE ARE ONLINE CLINIC</b></h6>
+    </div>
+
 </section>
+
 <!--HEADER-->
 
 <!--ABOUT-->
 
-<section id="about">
-    <div class="container element col-4">
-        <div class="container p-3 col-10 text-center">
-            <h1><b>ABOUT US</b></h1>
-            <hr>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et
-                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                aliquip
-                ex ea commodo consequat. </p>
-        </div>
+<section id="about" class="col-md-8 col-12 container">
+    <div class="container p-3 text-center">
+        <h1><b>ABOUT US</b></h1>
+        <hr>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et
+            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip
+            ex ea commodo consequat. </p>
     </div>
 </section>
 
@@ -76,23 +62,23 @@
 <!--ABOUT2-->
 
 
-<section id="about-2">
-    <div class="container col-6 ab-wrap">
-        <div class=" row text-center">
-            <div class="col-4 mb-4">
-                <img class="" src="image/left.png" alt="left">
-                <p>Lorem ipsum dolor sit amet</p>
-            </div>
-            <div class="col-4 mb-4">
-                <img src="image/middle.png" alt="middle">
-                <p>Lorem ipsum dolor sit amet</p>
-            </div>
-            <div class="col-4 mb-4">
-                <img src="image/right.png" alt="right">
-                <p>Lorem ipsum dolor sit amet</p>
-            </div>
-        </div>
+<section id="about-2" class="row justify-content-center">
+
+    <div class="col-md-3 text-center">
+        <img src="image/left.png" alt="left">
+        <p>Lorem ipsum dolor sit amet</p>
     </div>
+
+    <div class="col-md-3 text-center">
+        <img src="image/middle.png" alt="middle">
+        <p>Lorem ipsum dolor sit amet</p>
+    </div>
+
+    <div class="col-md-3 text-center">
+        <img src="image/right.png" alt="right">
+        <p>Lorem ipsum dolor sit amet</p>
+    </div>
+
 </section>
 
 
@@ -103,34 +89,30 @@
 
 
 <section id="slider">
-
-    <div class="col-6 container text-center justify-content-center">
-        <div class="container  slider-text">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-                et dolore magna aliqua.</p>
-        </div>
+    <div class="col-12 text-center">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+            et dolore magna aliqua.</p>
     </div>
-    <div id="img-slider" class="container col-6 slider-wrapper justify-content-center">
+    <div id="img-slider">
         <img src="" class="imghidden">
-        <img src="image/brouser_1.png" class="container left-photo photo px-0">
-        <img src="image/brouser_2.png" class="container center-photo photo px-0">
-        <img src="image/brouser_3.png" class="container right-photo photo px-0">
+        <img src="image/brouser_1.png" class="img-fluid left-photo photo">
+        <img src="image/brouser_2.png" class="img-fluid center-photo photo">
+        <img src="image/brouser_3.png" class="img-fluid right-photo photo">
     </div>
-    <div>
-        <div class="sli-nav container text-center justify-content-center">
-            <a class="prev" onclick="plusSlides(-1)"><img src="image/arrow_prev.png" alt="prev"></a>
-            <a class="next" onclick="plusSlides(1)"><img src="image/arrow_next.png" alt="next"></a>
-        </div>
+    <div id="navi-sli" class="container row col-12 text-center">
+        <a class="prev col-4" onclick="plusSlides(-1)"><img src="image/arrow_prev.png" alt="prev"></a>
+        <a class="next col-4" onclick="plusSlides(1)"><img src="image/arrow_next.png" alt="next"></a>
     </div>
 </section>
 
 
 <!--SLIDER-->
 
-<section id="Personal">
-    <div class="container col-6 text-center justify-content-center">
-        <div class="container">
-            <img src="image/photo.png" alt="photo">
+<section id="personal">
+    <div class="photo-slider col-12 text-center">
+
+        <div class="photo-slide">
+            <img src="image/photo.png" style="width:50%" alt="photo">
             <h1>Alice G. Mullins</h1>
             <h2>General Physician</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
@@ -138,112 +120,113 @@
                 dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
                 aliquip
                 ex ea commodo consequat.</p>
-            <a class="active" href="#">
-                <div class="round"></div>
-            </a>
-            <a href="#">
-                <div class="round"></div>
-            </a>
-            <a href="#">
-                <div class="round"></div>
-            </a>
         </div>
+        <div class="photo-slide">
+            <img src="image/photo.png" style="width:50%" alt="photo">
+            <h1>Alice G. Mullins</h1>
+            <h2>Assistant</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip
+                ex ea commodo consequat.</p>
+        </div>
+        <div class="photo-slide">
+            <img src="image/photo.png" style="width:50%" alt="photo">
+            <h1>Alice G. Mullins</h1>
+            <h2>Medic</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip
+                ex ea commodo consequat.</p>
+        </div>
+    </div>
+    <div style="text-align: center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
     </div>
 </section>
 
 <section id="services">
-    <div class="container p-0">
-        <div class="col-6 p-0">
-            <div class="text-services">
-                <h1>Lorem ipsum dolor sit amet</h1>
-                <h2>Sed do eiusmod tempor</h2>
+    <div class="col-4 offset-1">
+        <div class="text-services">
+            <h1>Lorem ipsum dolor sit amet</h1>
+            <h2>Sed do eiusmod tempor</h2>
 
-                <p>Sed ut perspiciatis unde omnis iste natus error sit volupta
-                    accusantium doloremque laudantium.</p>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit volupta
+                accusantium doloremque laudantium.</p>
 
-                <p>Totam rem aperiam, eaque ipsa quae ab illo inventore
-                    veritatis et quasi architecto beatae.</p>
+            <p>Totam rem aperiam, eaque ipsa quae ab illo inventore
+                veritatis et quasi architecto beatae.</p>
 
-                <p>Nemo enim ipsam voluptatem quia voluptas sit aspern
-                    atur aut odit aut fugit, sed quia consequuntur magni dolores
-                    eos qui ratione voluptatem sequi nesciunt.</p>
-            </div>
+            <p>Nemo enim ipsam voluptatem quia voluptas sit aspern
+                atur aut odit aut fugit, sed quia consequuntur magni dolores
+                eos qui ratione voluptatem sequi nesciunt.</p>
         </div>
     </div>
 </section>
 
-
 <section id="contacts">
-    <div class="container p-0">
-        <div class="row col-12 ">
-            <div class=" container p-0 col-6">
-                <a class="logo" href="#"><img src="image/logo.png" alt="logo"></a>
-                <div class="social">
-                    <a class="facebook" href="#"></a>
-                    <a class="twitter" href="#"></a>
-                    <a class="pinterest" href="#"></a>
-                    <a class="instagram" href="#"></a>
-                </div>
-                <div class="contacts-text">
+    <div class="col-md-12 row">
+        <div class="col-12 offset-1 col-md-5">
+            <a class="logo" href="#"><img src="image/logo.png" alt="logo"></a>
+            <div class="social">
+                <a class="facebook" href="#"></a>
+                <a class="twitter" href="#"></a>
+                <a class="pinterest" href="#"></a>
+                <a class="instagram" href="#"></a>
+            </div>
+            <div class="contacts-text">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                    labore
+                    et
+                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip
+                    ex ea commodo consequat.</p>
+                <br/>
+                <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                    pariatur.
+                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+                    id
+                    est
+                    laborum</p>
+            </div>
+        </div>
+        <!--LATEST POST-->
+        <div class="col-12 offset-1 col-md-5">
+            <h1>Latest Post</h1>
+            <div class="row ">
+                <img class="rect col-3" src="image/rect.png" alt="rect">
+                <div class="col-9">
+                    <h5>Lorem ipsum dolor sit amet cons...</h5>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
                         labore
                         et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                        aliquip
-                        ex ea commodo consequat.</p>
-                    <br/>
-                    <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                        pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
-                        id
-                        est
-                        laborum</p>
+                        dolore magna aliqua.</p>
                 </div>
             </div>
-
-
-            <!--LATEST POST-->
-            <div class="container col-6" id="lat-post">
-                <h1>Latest Post</h1>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="img-post"></div>
-                        <div class="col-8">
-                            <h2>Lorem ipsum dolor sit amet cons...</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                ut
-                                labore et dolore magna aliqua.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="img-post"></div>
-                        <div class="col-8">
-                            <h2>Lorem ipsum dolor sit amet cons...</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                ut
-                                labore et dolore magna aliqua.</p>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="container">
-                    <div class="row">
-                        <div class="img-post"></div>
-                        <div class="col-8">
-                            <h2>Lorem ipsum dolor sit amet cons...</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                                ut
-                                labore et dolore magna aliqua.</p>
-                        </div>
-                    </div>
+            <div class="row ">
+                <img class="rect col-3" src="image/rect.png" alt="rect">
+                <div class="col-9">
+                    <h5>Lorem ipsum dolor sit amet cons...</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</p>
                 </div>
             </div>
-        </div>
-    </div>
+            <div class="row ">
+                <img class="rect col-3" src="image/rect.png" alt="rect">
+                <div class="col-8">
+                    <h5>Lorem ipsum dolor sit amet cons...</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore
+                        et
+                        dolore magna aliqua.</p>
+                </div>
+            </div>
 </section>
 <!--LATEST POST-->
 
--- js/js.js
+++ js/js.js
@@ -15,22 +15,60 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-/**/
-
-
 function showSlides(n) {
-    if (n > 1) {
+    if (slideIndex > 1) {
         imgNumeric[0].src = imgNumeric[1].src;
         imgNumeric[1].src = imgNumeric[2].src;
         imgNumeric[2].src = imgNumeric[3].src;
         imgNumeric[3].src = imgNumeric[0].src;
-        n = 1;
+        slideIndex = 1;
     }
-    if (n < 1) {
+    if (slideIndex < 1) {
+        imgNumeric[0].src = imgNumeric[3].src;
         imgNumeric[3].src = imgNumeric[2].src;
         imgNumeric[2].src = imgNumeric[1].src;
         imgNumeric[1].src = imgNumeric[0].src;
-        imgNumeric[0].src = imgNumeric[3].src;
-        n = 1;
+        slideIndex = 1;
     }
+}
+/*MENU*/
+/**/
+$('#menu').click(function () {
+    if ($('#menu-open').css("display") == "none") {
+        $("#menu-open").css("display", "block");
+    } else {
+        $('#menu-open').css("display", "none");
+    }
+});
+/**/
+/*MENU*/
+
+
+var slideNum = 1;
+showPic(slideNum);
+
+// Next/previous controls
+function addNumslides(n) {
+    showPic(slideNum += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showPic(slideNum = n);
+}
+
+function showPic(n) {
+    var i;
+    var slides = document.getElementsByClassName("photo-slide");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideNum = 1}
+    if (n < 1) {slideNum = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideNum-1].style.display = "block";
+    dots[slideNum-1].className += " active";
 }
